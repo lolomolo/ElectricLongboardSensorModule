752d74575df5b5a7ba956e7402eb5b08bf5ce4ee
lolomolo <andreev.danny@gmail.com>
2017-11-29 17:04:12 -0600

    .m .wl

---
+++ Ev Sensor Module/Data/FinalTest/Test.m
@@ -0,0 +1,16 @@
+(* ::Package:: *)
+
+(* ::Input:: *)
+(*SetDirectory[NotebookDirectory[]];*)
+
+
+(* ::Input:: *)
+(*NotebookDirectory[]*)
+
+
+(* ::Input:: *)
+(*toBeExported=ToString[NotebookDirectory[]];*)
+
+
+(* ::Input:: *)
+(*Export["test.txt",toBeExported]*)
